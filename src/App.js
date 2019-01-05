3ae0aa0c571215d28934b14bc5d4b86f9f2d2719
Ari Perkkio <ari.perkkio@gmail.com>
2019-01-05 10:29:34 +0000

    Simplify App component tree

--- src/App.js
+++ src/App.js
@@ -13,34 +13,40 @@ const Dashboard = lazy(() => import(/* webpackChunkName: "dashboard" */ './views
 
 const App = () => {
     return (
-        <HashRouter>
-            <ThemeContextProvider>
-                <AuthContextProvider>
-                    <NavigationHeader />
-                    <Suspense fallback={<div />}>
-                        <Switch>
-                            <Route exact path="/signup" component={Signup} />
-                            <Route exact path="/login" component={Login} />
-
-                            {/* Routes requiring authentication */}
-                            <Route path='/auth' render={({ match: { path: auth } }) =>
-                                <Authenticator>
-                                    <DataContextProvider {...{ reducer, initialState }}>
-                                        <Switch>
-                                            <Route exact path={`${auth}(/home|/)`} component={Home} />
-                                            <Route exact path={`${auth}/dashboard`} component={Dashboard} />
-                                            <Redirect to={`${auth}/`} />
-                                        </Switch>
-                                    </DataContextProvider>
-                                </Authenticator>} />
-
-                            <Redirect to={'/login'} />
-                        </Switch>
-                    </Suspense>
-                </AuthContextProvider>
-            </ThemeContextProvider>
-        </HashRouter>
+        <RootContext>
+            <NavigationHeader />
+            <Suspense fallback={<div />}>
+                <Switch>
+                    <Route exact path="/signup" component={Signup} />
+                    <Route exact path="(/login|/)" component={Login} />
+                    <AuthenticatedRoutes />
+                    <Redirect to={'/'} />
+                </Switch>
+            </Suspense>
+        </RootContext>
     );
 };
+
+const RootContext = ({ children }) =>
+    <HashRouter>
+        <ThemeContextProvider>
+            <AuthContextProvider>
+                {children}
+            </AuthContextProvider>
+        </ThemeContextProvider>
+    </HashRouter>;
+
+const AuthenticatedRoutes = () =>
+    <Route path='/auth' render={({ match: { path: auth } }) =>
+        <Authenticator>
+            <DataContextProvider {...{ reducer, initialState }}>
+                <Switch>
+                    <Route exact path={`${auth}(/home|/)`} component={Home} />
+                    <Route exact path={`${auth}/dashboard`} component={Dashboard} />
+                    <Redirect to={`${auth}/`} />
+                </Switch>
+            </DataContextProvider>
+        </Authenticator>} />;
+
 
 export default App;
